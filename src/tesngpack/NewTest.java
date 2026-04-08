f800cf725898699391c5bdd37069e4ac860d888e
Admin <suryawanshireshma8@gmail.com>
2026-04-08 16:46:15 +0530

    first commit

---
+++ src/tesngpack/NewTest.java
@@ -0,0 +1,28 @@
+package tesngpack;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("Reshma");
+	  
+	  String expected = "Hello TestNG";
+	    String actual = "Hello TestNG";
+	    Assert.assertEquals(actual, expected, "The actual and expected strings should match");
+	    System.out.println("Test executed successfully!");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Hi");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("GoodMorning");
+  }
+
+}
